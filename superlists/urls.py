4bb0e805186221961d088bda88396413959708c9
Luna <lunacodes@gmail.com>
2022-12-12 17:11:55 -0500

    Convert URLs to re_path

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from lists.views import HomeView, ListView, NewListView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("", views.home_page, name="home"),
     path("", HomeView.as_view(), name="home"),
-    path("lists/<new>", NewListView.as_view(), name="view_list"),
-    path("lists/<list>", ListView.as_view(), name="view_list"),
+    path("lists/new", NewListView.as_view(), name="view_list"),
+    re_path(r"^lists/(.+)/$", ListView.as_view(), name="view_list"),
 ]
